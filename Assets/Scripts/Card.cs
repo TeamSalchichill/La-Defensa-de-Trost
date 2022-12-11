4659c1d91f8dd6dac181efecd17a82cba826097f
DavidPS17 <davidps155@gmail.com>
2022-12-11 16:35:22 +0100

    GOLD MASTER

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -317,8 +317,6 @@ public class Card : MonoBehaviour
 
     public void SelectTowerCard()
     {
-        border.rectTransform.sizeDelta -= new Vector2(25, 25);
-
         SoundManager.instance.SoundSelection(4, 0.5f);
 
         if (iter == 0)
